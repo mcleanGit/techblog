9f0fbc8f9a9233a2480e407a7351c7dfd21aed52
Don McLean <mclean@utoronto.ca>
2022-05-19 13:26:24 -0400

    drafts user post comment index for models

---
+++ models/index.js
@@ -0,0 +1,46 @@
+// models index.js developed from module
+// import all models
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+// create associations
+User.hasMany(Post, {
+  foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
+
+User.belongsToMany(Post, {
+  foreignKey: 'post_id',
+  onDelete: 'SET NULL'
+});
+
+Post.belongsToMany(User, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id',
+  onDelete: 'SET NULL'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote, Comment };
