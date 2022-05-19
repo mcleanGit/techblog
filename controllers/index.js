368dd1f94567d7189a49e0feeb1b58fbef260a3d
Don McLean <mclean@utoronto.ca>
2022-05-19 16:12:03 -0400

    drafts for all controllers

---
+++ controllers/index.js
@@ -0,0 +1,12 @@
+// controllers/index.js based on module, noting api, home and dashboard routes
+const router = require('express').Router();
+
+const apiRoutes = require('./api/');
+const homeRoutes = require('./home-routes.js');
+const dashboardRoutes = require('./dashboard-routes.js');
+
+router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes);
+router.use('/api', apiRoutes);
+
+module.exports = router;
