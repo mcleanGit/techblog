cce4635ac3208facca2a7823869e0e29027d7fc2
Don McLean <mclean@utoronto.ca>
2022-05-26 11:21:09 -0400

    modified from module

---
+++ public/javascript/add-post.js
@@ -0,0 +1,26 @@
+// add-post.js based on module, url to post_text etc.
+async function newFormHandler(event) {
+ event.preventDefault();
+
+ const title = document.querySelector('input[name="post-title"]').value;
+ const post_url = document.querySelector('input[name="post_text"]').value;
+
+ const response = await fetch(`/api/posts`, {
+   method: 'POST',
+   body: JSON.stringify({
+     title,
+     post_text
+   }),
+   headers: {
+     'Content-Type': 'application/json'
+   }
+ });
+
+ if (response.ok) {
+   document.location.replace('/dashboard');
+ } else {
+   alert(response.statusText);
+ }
+}
+
+document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
